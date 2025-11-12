6f7366bc4fc7ae2b1b085649e2b0f3db71aab16d
hyphenator35 <huntergarcia661@gmail.com>
2025-11-12 13:20:54 +0000

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+  return a + b
 
+def sub(a, b):
+  return a - b
 
+def mul(a, b):
+  if a == 0:
+    raise ZeroDivisionError
+  else:
+    return a * b
+
+def div(a, b):
+  return b / a
+
+def log(a, b):
+  if a <= 1 or b < 1:
+    raise ValueError
+  else:
+    return math.log(b, a)
+
+def exp(a, b):
+  return math.exp(a, b)
